6a20de12e17a30678124274674181cb87494a353
Ashley <ux24ii240@ux.edu.mx>
2026-03-23 20:39:18 -0600

    Práctica en clase 20-Marzo-2026

---
+++ src/main/java/com/mx/curso/Unidad2/SensorIA.java
@@ -0,0 +1,73 @@
+package com.mx.curso.Unidad2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class SensorIA {
+    protected String modelo;
+    protected double consumoEnergia;
+
+    public SensorIA(String modelo, double consumoEnergia) {
+        this.modelo = modelo;
+        this.consumoEnergia = consumoEnergia;
+    }
+
+    public abstract void leerDatos();
+}
+
+class SensorLidar extends SensorIA {
+
+    public SensorLidar(String modelo, double consumoEnergia) {
+        super(modelo, consumoEnergia);
+    }
+
+    @Override
+    public void leerDatos() {
+        System.out.println("Escaneando entorno 360°... Generando nube de puntos láser.");
+    }
+}
+
+class SensorVision extends SensorIA {
+
+    public SensorVision(String modelo, double consumoEnergia) {
+        super(modelo, consumoEnergia);
+    }
+
+    @Override
+    public void leerDatos() {
+        System.out.println("Capturando frame... Ejecutando segmentación semántica de objetos.");
+    }
+}
+
+class SensorUltrasonido extends SensorIA {
+
+    public SensorUltrasonido(String modelo, double consumoEnergia) {
+        super(modelo, consumoEnergia);
+    }
+
+    @Override
+    public void leerDatos() {
+        System.out.println("Enviando pulso sónico... Midiendo tiempo de rebote para proximidad.");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Hub de percepción
+        List<SensorIA> percepcionHub = new ArrayList<SensorIA>();
+
+        // Agregar sensores
+        percepcionHub.add(new SensorLidar("Lidar-X1", 15.5));
+        percepcionHub.add(new SensorVision("Cam-HD", 10.2));
+        percepcionHub.add(new SensorUltrasonido("UltraSound-Pro", 5.8));
+
+        // Demostración de Polimorfismo
+        System.out.println("=== Iniciando lectura de sensores ===");
+
+        for (SensorIA sensor : percepcionHub) {
+            sensor.leerDatos(); // Polimorfismo en acción
+        }
+    }
+}
